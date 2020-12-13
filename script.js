a2692cc7424419adb221469afc3b7212d13f146d
Esiena Ekwofia <eekwofia@gmail.com>
2020-12-13 18:00:13 -0500

    was able to get weather data to display on page.

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let currentTime = luxon.DateTime.local();
-  $(".currentDay").text(luxon.DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' }));
-  
+$(".currentDay").text(
+  luxon.DateTime.local().toLocaleString({
+    weekday: "long",
+    month: "long",
+    day: "2-digit",
+  })
+);
 
 var APIKey = "5b6d33dc8f643284870e57c82d7b049d";
 
@@ -25,12 +30,28 @@ $("#searchBtn").on("click", function (event) {
   }).then(function (response) {
     // Display response in the console log
     console.log(response);
+    var citiesTable = document.getElementById("citiesTable");
+  $(citiesTable).append("<tr>");
+  $("<td>").text(response.Name);
+
+  var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+    $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+  $(".wind").text("Wind Speed: " + response.wind.speed);
+  $(".humidity").text("Humidity: " + response.main.humidity);
   });
 
-  // to append searched cities to a table 
-  var citiesRow= $("<tr>")
-  var searchedCities = $("<td>").text(response.Name);
-  citiesRow.append(searchedCities)
+  
 
+  // // append a row to the table, and then append a cell to the row.
+  // var citiesTable = document.getElementById("citiesTable");
+  // $(citiesTable).append("<tr>");
+  // $("<td>").text(response.Name);
 
+  // var tempF = (response.main.temp - 273.15) * 1.8 + 32;
+
+  // $("#weathertron").text("Temperature (F) " + tempF.toFixed(2));
+
+//   $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+//   $(".wind").text("Wind Speed: " + response.wind.speed);
+//   $(".humidity").text("Humidity: " + response.main.humidity);
 });
